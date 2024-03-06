e3b4ae87594009ea2877fe73920c3a151ba506ba
lauPB07 <laurapiresberclaz@gmail.com>
2024-03-06 20:54:13 +0100

    feat : page connexion + sont css

---
+++ vue/connexion.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link rel="stylesheet" href="../assets/css/connexion.css">
+    <title>Connexion</title>
+</head>
+<body>
+<section>
+    <div class="form-box" >
+        <div class="form-value">
+            <form action="">
+                <h2>Connexion</h2>
+                <div class="inputbox">
+                    <ion-icon name="mail-outline"></ion-icon>
+                    <input type="email" name="email" required>
+                    <label for="">Email</label>
+                </div>
+                <div class="inputbox">
+                    <ion-icon name="lock-closed-outline"></ion-icon>
+                    <input type="password" name="mdp" required>
+                    <label for="">Mot De Passe</label>
+                </div>
+                <div class="forget">
+                    <label for=""><input type="checkbox"><a href="#">Mot De Passe Oublié</a></label>
+                </div>
+                <boutton>Connexion</boutton>
+                <div class="register">
+                    <p>Je n'ai pas de compte <a href="#">Inscription</a></p>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</section>
+</body>
+</html>
+
