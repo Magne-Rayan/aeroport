91ba908832a8ab228717d535f187b922c248b806
lauPB07 <laurapiresberclaz@gmail.com>
2024-03-06 21:28:22 +0100

    feat : ajout du traitement utilisteur

---
+++ src/controleur/TraitementUtilisateur.php
@@ -0,0 +1,11 @@
+<?php
+include '../bdd/Bdd.php';
+include '../model/Utilisateur.php';
+
+if(array_key_exists("Connexion",$_POST)){
+    $user = new Utilisateur([
+        "email" =>$_POST['email'],
+        "mdp" =>$_POST['mdp'],
+    ]);
+    $user->connexion();
+}
