5a8203e1242f9701a2a90cce1c814a8d4b674ffe
lauPB07 <laurapiresberclaz@gmail.com>
2024-03-06 23:08:19 +0100

    fix : modification du tratementutilisateur, ajout de l'inscription dans le tratement

--- src/controleur/TraitementUtilisateur.php
+++ src/controleur/TraitementUtilisateur.php
@@ -8,4 +8,13 @@ if(array_key_exists("Connexion",$_POST)){
         "mdp" =>$_POST['mdp'],
     ]);
     $user->connexion();
+}elseif (array_key_exists("inscription",$_POST)){
+    $user = new Utilisateur([
+        "nom" =>$_POST['nom'],
+        "prenom" =>$_POST['prenom'],
+        "age" =>$_POST['age'],
+        "email" =>$_POST['email'],
+        "mdp" =>$_POST['mdp'],
+    ]);
+    $user->inscription();
 }
