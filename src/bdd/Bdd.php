bb5001fbe00824d8e4d6b12ad01c6eac102f40af
Magne Rayan <r.magne@lprs.fr>
2024-03-06 17:55:28 +0100

    feat : ajout class Bdd

--- src/bdd/Bdd.php
+++ src/bdd/Bdd.php
@@ -5,4 +5,21 @@ namespace bdd;
 class Bdd
 {
 
+    private $bdd;
+    private $serveur = "localhost";
+    private $nomBdd = "aeroport";
+    private $username = "";
+    private $password = "";
+
+
+    public function __construct()
+    {
+        $this->bdd = new PDO('mysql:host=' . $this->serveur . ';dbname=' . $this->nomBdd, $this->username, $this->password);
+    }
+
+    public function getBdd()
+    {
+        return $this->bdd;
+    }
+
 }
