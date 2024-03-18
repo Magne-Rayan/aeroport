856e22a27d139b660dd8fb51acd3ea5648da4b7d
lauPB07 <laurapiresberclaz@gmail.com>
2024-03-18 14:29:35 +0100

    feat : checkbox pour attribuer les roles

--- src/controleur/TraitementUtilisateur.php
+++ src/controleur/TraitementUtilisateur.php
@@ -10,12 +10,27 @@ if(array_key_exists("Connexion",$_POST)){
     $user->connexion();
 
 }else if (array_key_exists('Inscription',$_POST)){
-    $user = new Utilisateur([
-        "nom" =>$_POST['nom'],
-        "prenom" =>$_POST['prenom'],
-        "date" =>$_POST['dateNaissance'],
-        "email" =>$_POST['email'],
-        "mdp" =>$_POST['mdp'],
-    ]);
-    $user->inscription();
+    if(isset($_POST["pilote"])){
+        $user = new Utilisateur([
+            "nom" =>$_POST['nom'],
+            "prenom" =>$_POST['prenom'],
+            "date" =>$_POST['dateNaissance'],
+            "email" =>$_POST['email'],
+            "mdp" =>$_POST['mdp'],
+            "role" =>1,
+        ]);
+        $user->inscription();
+    }if(isset($_POST["client"])){
+        $user = new Utilisateur([
+            "nom" =>$_POST['nom'],
+            "prenom" =>$_POST['prenom'],
+            "date" =>$_POST['dateNaissance'],
+            "email" =>$_POST['email'],
+            "mdp" =>$_POST['mdp'],
+            "role" =>2,
+        ]);
+        $user->inscription();
+    }
+
+
 }
