6bac2033140701dd2ee3856580ae21209ccbf79c
Magne Rayan <r.magne@lprs.fr>
2024-03-18 16:07:17 +0100

    feat : datatable pilote pour la page admin

--- vue/admin.php
+++ vue/admin.php
@@ -1,31 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-<<<<<<< HEAD
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Tour & Travel</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <link href="css/family.css" rel="stylesheet">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Tour & Travel</title>
-        <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
-        <link href="../assets/css/style.css" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
-        <link href="../assets/css/element.css" rel="stylesheet">
-        <link href="css/ken-burns.css" rel="stylesheet" type="text/css" media="all" />
-        <link type="text/css" rel="stylesheet" href="../assets/css/animate.css">
-        <script src="../assets/js/jquery-2.1.1.min.js"></script>
-        <script src="../assets/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="js/codebase/dhtmlxcalendar.css"/>
-        <script src="../assets/js/codebase/dhtmlxcalendar.js"></script>
-        <script>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/family.css" rel="stylesheet">
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>Tour & Travel</title>
+            <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+            <link href="../assets/css/style.css" rel="stylesheet">
+            <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+            <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+            <link href="../assets/css/element.css" rel="stylesheet">
+            <link href="css/ken-burns.css" rel="stylesheet" type="text/css" media="all" />
+            <link type="text/css" rel="stylesheet" href="../assets/css/animate.css">
+            <script src="../assets/js/jquery-2.1.1.min.js"></script>
+            <script src="../assets/js/bootstrap.min.js"></script>
+            <link rel="stylesheet" type="text/css" href="js/codebase/dhtmlxcalendar.css"/>
+            <script src="../assets/js/codebase/dhtmlxcalendar.js"></script>
+            <script>
             var myCalendar;
             function doOnLoad() {
                 myCalendar = new dhtmlXCalendarObject(["cal_1","cal_2", "cal_3", "cal_4", "cal_5", "cal_6", "cal_7", "cal_8"]);
